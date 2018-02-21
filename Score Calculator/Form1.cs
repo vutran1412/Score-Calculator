8c0f246b8a3318c338a68d551678ed95a8f178fa
Vu Tran <ef4843ir@go.minneapolis.edu>
2018-02-21 16:26:05 -0600

    Added data validation, text box cannot be empty, must be an integer within the ranges of 0 to 100

--- Score Calculator/Form1.cs
+++ Score Calculator/Form1.cs
@@ -22,24 +22,83 @@ namespace Score_Calculator
             int scoreCount = 0;
             int scoreTotal = 0;
             int scoreAverage = 0;
-
-            int score = Convert.ToInt32(txtScore.Text);
-
-            scores.Add(score);
-
-            foreach (int s in scores)
+            try
             {
-                scoreCount++;
-                lblScoreCount.Text = scoreCount.ToString();
-                
-                scoreTotal += s;
-                lblScoreTotal.Text = scoreTotal.ToString();
+                if (IsValidData())
+                {
+                    int score = Convert.ToInt32(txtScore.Text);
 
-                scoreAverage = scoreTotal / scoreCount;
-                lblAverage.Text = scoreAverage.ToString();
+                    scores.Add(score);
+
+                    foreach (int s in scores)
+                    {
+                        scoreCount++;
+                        lblScoreCount.Text = scoreCount.ToString();
+
+                        scoreTotal += s;
+                        lblScoreTotal.Text = scoreTotal.ToString();
+
+                        scoreAverage = scoreTotal / scoreCount;
+                        lblAverage.Text = scoreAverage.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "" + "\n\n" +
+                    ex.GetType().ToString() + "\n" +
+                    ex.StackTrace, "Exception");
             }
             
         }
+
+        public bool IsValidData()
+        {
+            return
+                IsPresent(txtScore, "Test Score") &&
+                IsInteger(txtScore, "Test Score") &&
+                IsWithinRange(txtScore, "Test Score", 0, 100);
+        }
+
+        public bool IsPresent(TextBox textBox, string name)
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field and cannot be empty.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        public bool IsInteger(TextBox textBox, String name)
+        {
+            int number = 0;
+            if (int.TryParse(textBox.Text, out number))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be an integer,", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+        }
+
+        public bool IsWithinRange(TextBox textbox, string name, int min, int max)
+        {
+            int number = Convert.ToInt16(textbox.Text);
+            if (number < min || number > max)
+            {
+                MessageBox.Show(name + " must be between " + min
+                    + " and " + max + ".", "Entry Error");
+                textbox.Focus();
+                return false;
+            }
+            return true;
+        }
+
 
         private void btnDisplayScores_Click(object sender, EventArgs e)
         {
